0123c52df819dd3d2cce82a593e9a94b3bfa81c4
Luke Parker <lukeparker5132@gmail.com>
2024-04-29 01:14:32 -0400

    Clean div rem

--- crypto/divisors/src/poly.rs
+++ crypto/divisors/src/poly.rs
@@ -148,7 +148,10 @@ impl<F: PrimeField + From<u64>> Mul<F> for Poly<F> {
 
 impl<F: PrimeField + From<u64>> Poly<F> {
   fn shift_by_x(mut self, power_of_x: usize) -> Self {
-    debug_assert_ne!(power_of_x, 0);
+    if power_of_x == 0 {
+      return self;
+    }
+
     // Shift up every x coefficient
     for _ in 0 .. power_of_x {
       self.x_coefficients.insert(0, F::ZERO);
@@ -180,7 +183,10 @@ impl<F: PrimeField + From<u64>> Poly<F> {
   }
 
   fn shift_by_y(mut self, power_of_y: usize) -> Self {
-    debug_assert_ne!(power_of_y, 0);
+    if power_of_y == 0 {
+      return self;
+    }
+
     // Shift up every y coefficient
     for _ in 0 .. power_of_y {
       self.y_coefficients.insert(0, F::ZERO);
@@ -228,13 +234,19 @@ impl<F: PrimeField + From<u64>> Mul for Poly<F> {
 }
 
 impl<F: PrimeField + From<u64>> Poly<F> {
-  // Perform multiplication mod `modulus`
-  pub(crate) fn mul_mod(self, other: Self, modulus: &Self) -> Self {
+  /// Perform multiplication mod `modulus`.
+  pub fn mul_mod(self, other: Self, modulus: &Self) -> Self {
     ((self % modulus) * (other % modulus)) % modulus
   }
 
   /// Perform division, returning the result and remainder.
-  pub fn div_rem(self, denominator: &Self) -> (Self, Self) {
+  ///
+  /// Panics upon division by zero.
+  pub fn div_rem(self, divisor: Self) -> (Self, Self) {
+    // Ensure the divisor is tidy
+    let divisor = divisor.tidy();
+
+    // The leading y coefficient and associated x coefficient.
     let leading_y = |poly: &Self| -> (_, _) {
       if poly.y_coefficients.len() > poly.yx_coefficients.len() {
         (poly.y_coefficients.len(), 0)
@@ -245,83 +257,62 @@ impl<F: PrimeField + From<u64>> Poly<F> {
       }
     };
 
-    let leading_x = |poly: &Self| -> (_, _) {
-      if poly.yx_coefficients.is_empty() {
-        if !poly.x_coefficients.is_empty() {
-          return (0, poly.x_coefficients.len());
-        }
-        return (poly.y_coefficients.len(), 0);
-      }
-
-      let mut longest_yx = 0;
-      for i in 0 .. poly.yx_coefficients.len() {
-        if poly.yx_coefficients[i].len() > poly.yx_coefficients[longest_yx].len() {
-          longest_yx = i;
-        }
-      }
-
-      if poly.x_coefficients.len() > poly.yx_coefficients[longest_yx].len() {
-        (0, poly.x_coefficients.len())
-      } else {
-        (longest_yx + 1, poly.yx_coefficients[longest_yx].len())
-      }
-    };
-
-    let y_denom = leading_y(denominator).0 != 0;
-
-    let mut q = Poly::zero();
-    let mut r = self;
-    while {
-      let ((y_a, x_a), (y_b, x_b)) = if y_denom {
-        (leading_y(&r), leading_y(denominator))
-      } else {
-        (leading_x(&r), leading_x(denominator))
-      };
-      (r != Poly::zero()) && (y_a >= y_b) && (x_a >= x_b)
-    } {
-      let ((y_a, x_a), (y_b, x_b)) = if y_denom {
-        (leading_y(&r), leading_y(denominator))
-      } else {
-        (leading_x(&r), leading_x(denominator))
-      };
-
-      let coeff = |poly: &Self, y: usize, x: usize| -> F {
-        if (y == 0) && (x == 0) {
-          poly.zero_coefficient
-        } else if x == 0 {
-          poly.y_coefficients[y - 1]
-        } else if y == 0 {
-          poly.x_coefficients[x - 1]
-        } else {
-          poly.yx_coefficients[y - 1][x - 1]
-        }
-      };
-
-      let coeff_a = coeff(&r, y_a, x_a);
-      let coeff_b = coeff(denominator, y_b, x_b);
-      let coeff = coeff_a * Option::<F>::from(coeff_b.invert()).expect("denominator wasn't tidy");
-      let y_c = y_a - y_b;
-      let x_c = x_a - x_b;
-
-      let mut t = Poly::zero();
-      if (y_c == 0) && (x_c == 0) {
-        t.zero_coefficient = coeff;
-      } else if x_c == 0 {
-        t.y_coefficients = vec![F::ZERO; y_c];
-        t.y_coefficients[y_c - 1] = coeff;
-      } else if y_c == 0 {
-        t.x_coefficients = vec![F::ZERO; x_c];
-        t.x_coefficients[x_c - 1] = coeff;
-      } else {
-        t.yx_coefficients = vec![vec![]; y_c];
-        t.yx_coefficients[y_c - 1] = vec![F::ZERO; x_c];
-        t.yx_coefficients[y_c - 1][x_c - 1] = coeff;
-      }
-      q = q + &t;
-      r = r.sub(t * denominator.clone());
+    let (div_y, div_x) = leading_y(divisor);
+    // If this divisor is actually a scalar, don't perform long division
+    if (div_y == 0) && (div_x == 0) {
+      return self * divisor.zero_coefficient.invert().unwrap();
     }
 
-    (q, r)
+    // Remove leading terms until the value is less than the divisor
+    let mut quotient: Poly<F> = Poly::zero();
+    let mut remainder = self.clone();
+    loop {
+      // If there's nothing left to divide, return
+      if remainder == Poly::zero() {
+        break;
+      }
+
+      let (rem_y, rem_x) = leading_y(&remainder);
+      if (rem_y < div_y) || (rem_x < div_x) {
+        break;
+      }
+
+      let get = |poly: &Poly<F>, y_pow: usize, x_pow: usize| -> F {
+        if (y_pow == 0) && (x_pow == 0) {
+          poly.zero_coefficient
+        } else if x_pow == 0 {
+          poly.y_coefficients[y_pow - 1]
+        } else if y_pow == 0 {
+          poly.x_coefficients[x_pow - 1]
+        } else {
+          poly.yx_coefficients[y_pow - 1][x_pow - 1]
+        }
+      };
+      let coeff_numerator = get(&remainder, rem_y, rem_x);
+      let coeff_denominator = get(divisor, div_y, div_x);
+
+      // We want coeff_denominator scaled by x to equal coeff_numerator
+      // x * d = n
+      // n / d = x
+      let mut quotient_term = Poly::zero();
+      // Because this is the coefficient for the leading term of a tidied polynomial, it must be
+      // non-zero
+      quotient_term.zero_coefficient = coeff_numerator * coeff_denominator.invert().unwrap();
+
+      // Add the necessary yx powers
+      let delta_y = rem_y - div_y;
+      let delta_x = rem_x - div_x;
+      let quotient_term = quotient_term.shift_by_y(delta_y).shift_by_x(delta_x);
+
+      let to_remove = quotient_term.clone() * divisor.clone();
+      debug_assert_eq!(get(&to_remove, rem_y, rem_x), coeff_numerator);
+
+      remainder = remainder - to_remove;
+      quotient = quotient + &quotient_term;
+    }
+    debug_assert_eq!((quotient.clone() * divisor.clone()) + &remainder, self);
+
+    (quotient, remainder)
   }
 }
 
