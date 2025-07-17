7bc00f24470b08f77d0bcd1c07731eec067638a3
Luke Parker <lukeparker5132@gmail.com>
2025-07-16 23:52:25 -0400

    Update bench to use clock cycles on x86/x86-64

--- crypto/helioselene/benches/helioselene.rs
+++ crypto/helioselene/benches/helioselene.rs
@@ -1,7 +1,5 @@
 #![allow(non_snake_case)]
 
-use std::time;
-
 use rand_core::OsRng;
 use helioselene::{
   group::{ff::Field, Group, GroupEncoding},
@@ -10,13 +8,23 @@ use helioselene::{
 
 macro_rules! run_bench {
   ($name: literal, $op:expr, $n_iters:expr) => {{
-    let start = time::Instant::now();
+    #[cfg(target_arch = "x86")]
+    let start = unsafe { core::arch::x86::_rdtsc() };
+    #[cfg(target_arch = "x86_64")]
+    let start = unsafe { core::arch::x86_64::_rdtsc() };
+    #[cfg(not(any(target_arch = "x86", target_arch = "x86_64")))]
+    let start = std::time::Instant::now();
     for _ in 0 .. $n_iters {
       let _ = core::hint::black_box($op);
     }
-    let time_to_run = (time::Instant::now() - start).as_millis();
+    #[cfg(target_arch = "x86")]
+    let time_to_run = unsafe { core::arch::x86::_rdtsc() } - start;
+    #[cfg(target_arch = "x86_64")]
+    let time_to_run = unsafe { core::arch::x86_64::_rdtsc() } - start;
+    #[cfg(not(any(target_arch = "x86", target_arch = "x86_64")))]
+    let time_to_run = (std::time::Instant::now() - start).as_millis();
 
-    println!("{} took {}ms", $name, time_to_run);
+    println!("{:<23} took {:>12} ticks", $name, time_to_run);
   }};
 }
 
@@ -30,9 +38,9 @@ fn main() {
   let A_S_bytes = A_S.to_bytes();
 
   run_bench!("Selene Point add", A_S + B_S, 2_000_000);
-  run_bench!("helioselene mul", a_h * b_h, 50_000_000);
+  run_bench!("helioselene mul", a_h * b_h, 2_000_000_000);
   run_bench!("helioselene invert", a_h.invert(), 200_000);
   run_bench!("Selene Point from_bytes", SelenePoint::from_bytes(&A_S_bytes), 100_000);
-  run_bench!("helioselene add", a_h + b_h, 200_000_000);
-  run_bench!("helioselene sub", a_h - b_h, 200_000_000);
+  run_bench!("helioselene add", a_h + b_h, 2_000_000_000);
+  run_bench!("helioselene sub", a_h - b_h, 2_000_000_000);
 }
